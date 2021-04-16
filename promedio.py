9e684a76cc6316593af98e181b410d92b9e05c74
guillermo6410 <80936698+guillermo6410@users.noreply.github.com>
2021-04-16 14:42:18 -0500

    Promedio de calificaciones

---
+++ promedio.py
@@ -0,0 +1,9 @@
+print("Promedio de calificaciones")
+print("Ingresa los resultados de las materias")
+matematicas=int(input("Ingresa calificacion matematicas:"))
+español=int(input("Ingresa calificacion español:"))
+fisica=int(input("Ingresa calificacion fisica:"))
+informatica=int(input("Ingresa calificacion informatica:"))
+quimica=int(input("Ingresa calificacion quimica:"))
+promedio=(matematicas+español+fisica+informatica+quimica)/5
+print("El promedio es de:", promedio)
