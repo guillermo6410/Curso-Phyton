ef1d3df6cccfbfa40773848c6aebee968b1d5af4
guillermo6410 <80936698+guillermo6410@users.noreply.github.com>
2021-05-26 00:39:15 -0500

    Inversion, capital, interes

---
+++ Ejercicio3_3.py
@@ -0,0 +1,11 @@
+def hallar_inversion():
+    cant_inv=int(input("ingrese la cantidad a invertir"))
+    interes_anual=float(input("ingrese el interes anual"))
+    num_años=int(input("ingrese el numero de años"))
+
+    interes_total=interes_anual*num_años
+    capital_invertido=cant_inv+cant_inv*interes_total
+
+    print(capital_invertido)
+
+hallar_inversion()       
